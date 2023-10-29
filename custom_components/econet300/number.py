58df0539ccfc21520c517bb603c4ac95504a09d2
ZZZ <zaapp1993@gmail.com>
2023-10-29 19:59:59 +0100

    test3

--- custom_components/econet300/number.py
+++ custom_components/econet300/number.py
@@ -66,8 +66,9 @@ class EconetNumber(EconetEntity, NumberEntity):
         self._attr_native_value = value
         self.async_write_ha_state()
 
-    async def async_update(self):
+    async def async_update(self) -> None:
         self._attr_value = self._api.get__param_value(self.entity_description.key)
+        self.async_write_ha_state()
 
     async def async_set_native_value(self, value: str) -> None:
         """Update the current value."""
