ece8e28094512a889f1716b3c872c971077c3625
ZZZ <zaapp1993@gmail.com>
2023-10-29 18:26:38 +0100

    .

--- custom_components/econet300/number.py
+++ custom_components/econet300/number.py
@@ -63,7 +63,9 @@ class EconetNumber(EconetEntity, NumberEntity):
 
     def _sync_state(self, value):
         """Sync state"""
-        self._attr_native_value = float(value)
+        self._attr_native_value = value
+        self._attr_native_min_value = value - 1
+        self._attr_native_max_value = value + 1
         self.async_write_ha_state()
 
     async def async_set_native_value(self, value: str) -> None:
